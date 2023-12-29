f0ce304ee732bc2d93142c6e03e4ce1dad2829a3
Muhammad Ehsan <muhammadehsanhassan781@gmail.com>
2023-12-29 11:03:23 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+<img class="logo-img" alt="Vue logo" src="./assets/ehsan8.jpeg">
+<HelloWorld msg="Welcome to Ehsan App" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    let a = 2;
+    let b = 5;
+    console.log("a + b = ", a + b);
+    return {};
+  },
+  beforeCreate() {
+    console.log("beforecreate parent");
+  },
+  Created() {
+    console.log("created parent");
+  },
+  beforeMount() {
+    console.log("beforeMount parent");
+  },
+  mounted() {
+    console.log("mounted parent");
+  },
+  beforeUpdate() {
+    console.log("Before update parent");
+  },
+  updated() {
+    console.log("updated parent");
+  },
+  beforeUnmount() {
+    console.log("before unmount parent");
+  },
+  unmounted() {
+    console.log("unmounted parent");
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.logo-img {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <div class="form">
+      <div>
+        <label for="username">Admin: </label>
+        <input id="username" type="text" v-model="inputText" />
+        <p class="you-typed">
+          Your Name is: "<span style="color: red">{{ inputText }}</span
+          >"
+        </p>
+      </div>
+      <div>
+        <label for="email">Email: </label>
+        <input id="email" type="email" v-model="email" />
+        <p class="you-typed">
+          Your Email is: "<span style="color: red">{{ email }}</span
+          >"
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    const inputText = ref("");
+    const email = ref("");
+    let a = 5;
+    let b = 5;
+    console.log("a + b = ", a + b);
+    return {
+      inputText,
+      email,
+    };
+  },
+  beforeCreate() {
+    console.log("beforecreate child");
+  },
+  Created() {
+    console.log("created child");
+  },
+  beforeMount() {
+    console.log("beforeMount child");
+  },
+  mounted() {
+    console.log("mounted child");
+  },
+  beforeUpdate() {
+    console.log("Before update child");
+  },
+  updated() {
+    console.log("updated child");
+  },
+  beforeUnmount() {
+    console.log("before unmount child");
+  },
+  unmounted() {
+    console.log("unmounted child");
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+.form {
+  width: 50%;
+  margin: auto;
+  padding: 1rem;
+}
+label {
+  font-weight: bold;
+}
+</style>
